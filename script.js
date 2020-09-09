92a2be1f00d5c93a93b0943db303b2f5edf8ff69
Harritone <yourrasik@gmail.com>
2020-09-09 23:30:43 +1000

    Operator precedence

--- script.js
+++ script.js
@@ -50,29 +50,67 @@
 // Basic operators
 //****************************************
 
-var year, ageJohn, ageMark;
-now = 2020;
-//Math operators
-ageJohn = 28;
-ageMark = 33;
+//var year, ageJohn, ageMark;
+//now = 2020;
+////Math operators
+//ageJohn = 28;
+//ageMark = 33;
+//
+//console.log(ageJohn);
+//console.log(ageMark);
+//console.log(now + 2);
+//console.log(now * 2);
+//console.log(now / 10);
+//
+//// Logical operators
+//
+//var johnOlder = ageJohn > ageMark;
+//console.log(johnOlder);
+//
+////typeof operator
+//
+//console.log(typeof johnOlder);
+//console.log(typeof ageMark);
+//console.log(typeof 'yeah');
+//var day;
+//console.log(typeof day);
+//var day = null;
+//console.log(typeof day);
 
-console.log(ageJohn);
-console.log(ageMark);
-console.log(now + 2);
-console.log(now * 2);
-console.log(now / 10);
+//****************************************
+// Operator precedence
+//****************************************
 
-// Logical operators
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
 
-var johnOlder = ageJohn > ageMark;
-console.log(johnOlder);
+//Multiple operators
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
 
-//typeof operator
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
 
-console.log(typeof johnOlder);
-console.log(typeof ageMark);
-console.log(typeof 'yeah');
-var day;
-console.log(typeof day);
-var day = null;
-console.log(typeof day);
+// Multiple assignments
+
+var x, y;
+x = y = (3 + 5) * 4 - 6; 
+console.log(x, y);
+
+// more operators
+
+x *= 2;
+console.log(x);
+
+x += 10;
+console.log(x);
+
+x++;
+console.log(x);
+
+x--;
+console.log(x);
