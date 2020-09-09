a1845902a1e8f335dad8a754e562ac8e44d59857
Harritone <yourrasik@gmail.com>
2020-09-09 22:13:57 +1000

    Varriables and data types

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello World!!!');
+// 1
+//console.log('Hello World!!!');
+//
+// 2
+var firstName = 'Ivan';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
