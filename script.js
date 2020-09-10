037078a7502a32b70f8676f157c03f66204ed78e
Harritone <yourrasik@gmail.com>
2020-09-10 23:58:36 +1000

    Coding challege 5 part 2

--- script.js
+++ script.js
@@ -624,5 +624,29 @@ john = {
   }
 }
 
+mark = {
+  bills: [77, 475, 110, 45],
+  calcTips: function() {
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++) {
+      var percentage;
+      var bill = this.bills[i];
+      if (bill < 100) {
+        percentage = .2;
+      } else if(bill >= 100 && bill < 300) {
+        percentage = .1;
+      } else {
+        percentage = .25;
+      }
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + bill * percentage;
+    }
+  }
+}
+
 john.calcTips();
-console.log(john);
+mark.calcTips();
+console.log(john, mark);
+
