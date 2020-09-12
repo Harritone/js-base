1f303f0c90f58ea120d6d3bc0d1f38b61f62e3a3
Harritone <yourrasik@gmail.com>
2020-09-12 17:54:08 +1000

    Object.create

--- advanced stuff/script.js
+++ advanced stuff/script.js
@@ -1,37 +1,57 @@
 // Function constructor
 
-var john = {
-  name: 'John',
-  yearOfBirth: 1990,
-  job: 'teacher'
-};
+//var john = {
+//  name: 'John',
+//  yearOfBirth: 1990,
+//  job: 'teacher'
+//};
 //console.log(john);
 
-var Person = function(name, yearOfBirth, job) {
-  this.name = name;
-  this.yearOfBirth = yearOfBirth;
-  this.job = job;
-  //this.calculateAge = function() {
-  //  console.log(2020 - this.yearOfBirth);
-  //};
-}
+//var Person = function(name, yearOfBirth, job) {
+//  this.name = name;
+//  this.yearOfBirth = yearOfBirth;
+//  this.job = job;
+//  //this.calculateAge = function() {
+//  //  console.log(2020 - this.yearOfBirth);
+//  //};
+//}
+//
+//Person.prototype.calculateAge = function() {
+//    console.log(2020 - this.yearOfBirth);
+//};
+//
+//Person.prototype.lastName = 'Smith';
+//
+//var john = new Person('John', 1990, 'teacher');
+////console.log(john);
+//
+//var jane = new Person('Jane', 1988, 'designer');
+//var mark = new Person('Mark', 1948, 'retired');
+//
+//john.calculateAge();
+//jane.calculateAge();
+//mark.calculateAge();
+//
+//console.log(john.lastName);
+//console.log(jane.lastName);
+//console.log(mark.lastName);
+//console.log(john);
 
-Person.prototype.calculateAge = function() {
+// Object.create
+
+var personProto = {
+  calculateAge: function() {
     console.log(2020 - this.yearOfBirth);
+  }
 };
 
-Person.prototype.lastName = 'Smith';
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
 
-var john = new Person('John', 1990, 'teacher');
-//console.log(john);
-
-var jane = new Person('Jane', 1988, 'designer');
-var mark = new Person('Mark', 1948, 'retired');
-
-john.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
-
-console.log(john.lastName);
-console.log(jane.lastName);
-console.log(mark.lastName);
+var jane = Object.create(personProto, {
+  name: { value: 'Jane' },
+  yearOfBirth: { value: 1969 },
+  job: { value: 'designer' }
+});
