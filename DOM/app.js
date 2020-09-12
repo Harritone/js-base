ad64cdcd6c676a688a58267ff3f809e9ae14636c
Harritone <yourrasik@gmail.com>
2020-09-12 11:27:52 +1000

    FInal changes for dice

--- DOM/app.js
+++ DOM/app.js
@@ -9,7 +9,7 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, prevDice;
 
 init();
 
@@ -17,21 +17,38 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
   if(gamePlaying) {
     //1. Random number
-    var dice = Math.floor(Math.random() * 6 ) +1;
-    //
+    var dice1 = Math.floor(Math.random() * 6 ) +1;
+    var dice2 = Math.floor(Math.random() * 6 ) +1;
     //2. display the result
-    var diceDOM = document.querySelector('.dice');
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
 
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+
     //3. Update the round score IF the rolled number was NOT a 1
-    if (dice !== 1) {
+
+    /*
+    if (dice === 6 && prevDice === 6) {
+      //Player loose score
+      scores[activePlayer] = 0;
+      document.querySelector('#score-' + activePlayer).textContent = '0';
+      nextPlayer();
+    } else if (dice !== 1) {
       //add score
       roundScore += dice;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
-
     } else {
       //Next player
+      nextPlayer();
+    }
+    prevDice = dice;
+    */
+
+    if (dice1 !== 1 && dice2 !== 1) {
+      roundScore += dice1 + dice2;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
       nextPlayer();
     }
   }
@@ -44,10 +61,16 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     scores[activePlayer] += roundScore;
     // Update the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    var input = document.querySelector('.final-score').value;
+    if (input) {
+      var finalScore = input;
+    } else {
+      var finalScore = 100;
+    }
     // check if player won the game
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= finalScore) {
+      hideDices();
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-      document.querySelector('.dice').style.display = 'none';
 
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
@@ -62,6 +85,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 function nextPlayer() {
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
   roundScore = 0;
+  prevDice = 0;
   document.getElementById('current-0').textContent = '0';
   document.getElementById('current-1').textContent = '0';
 
@@ -71,7 +95,7 @@ function nextPlayer() {
   //document.querySelector('.player-0-panel').classList.remove('active');
   //document.querySelector('.player-1-panel').classList.add('active');
 
-  document.querySelector('.dice').style.display = 'none';
+  hideDices();
 }
 
 document.querySelector('.btn-new').addEventListener('click', init)
@@ -82,8 +106,9 @@ function init() {
   activePlayer = 0;
   roundScore = 0;
   gamePlaying = true;
+  prevDice = 0;
 
-  document.querySelector('.dice').style.display = 'none';
+  hideDices();
 
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
@@ -96,4 +121,9 @@ function init() {
   document.querySelector('.player-0-panel').classList.remove('active');
   document.querySelector('.player-1-panel').classList.remove('active');
   document.querySelector('.player-0-panel').classList.add('active');
+}
+
+function hideDices() {
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 }
